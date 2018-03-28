3586fb9194ca80a003b33ea7197fa50a95dd2e1f
liheng-heng <hengli_15@126.com>
2018-03-28 16:26:02 +0800

    liheng

--- app/src/main/java/dialogloadding/qq986945193/com/dialogloadding/DialogThridUtils.java
+++ app/src/main/java/dialogloadding/qq986945193/com/dialogloadding/DialogThridUtils.java
@@ -14,10 +14,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
- * @author ：程序员小冰
- * @新浪微博 ：http://weibo.com/mcxiaobing
- * @GitHub: https://github.com/QQ986945193
- * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @author ：liheng
  */
 public class DialogThridUtils {
     /**
--- app/src/main/java/dialogloadding/qq986945193/com/dialogloadding/MainActivity.java
+++ app/src/main/java/dialogloadding/qq986945193/com/dialogloadding/MainActivity.java
@@ -9,10 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 /**
- * @author ：程序员小冰
- * @新浪微博 ：http://weibo.com/mcxiaobing
- * @GitHub: https://github.com/QQ986945193
- * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @author ：liheng
  */
 public class MainActivity extends AppCompatActivity {
     private Dialog mDialog;
